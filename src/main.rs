e13194fb6fafc5825a345c39ebbc09bee9b5433f
Vincent C. Mader <vincent.mader@icloud.com>
2024-04-15 18:00:59 +0200

    IMPR Define default font family

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,23 @@
+use stylist::css;
 use yew::prelude::*;
 use yew_client::services::routing::{switch, Route};
 use yew_router::prelude::*;
 
+const DEFAULT_FONT_FAMILY: &str = "Arial, sans-serif";
+
 #[function_component]
 fn App() -> Html {
+    let class = css!(
+        "
+            --DEFAULT_FONT_FAMILY: ${DEFAULT_FONT_FAMILY};
+
+            font-family: var(--DEFAULT_FONT_FAMILY);
+        ",
+        DEFAULT_FONT_FAMILY = DEFAULT_FONT_FAMILY
+    );
+
     html! {
-        <div>
+        <div {class}>
             <BrowserRouter>
                 <Switch<Route> render={switch} />
             </BrowserRouter>
